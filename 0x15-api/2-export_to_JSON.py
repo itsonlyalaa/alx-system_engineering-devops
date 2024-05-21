b3575e98d9316f954e62db0d8419cbb22daaaa40
Alaa Okair <alaaokair330@gmail.com>
2024-05-20 18:56:14 -0700

    tasks

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A script that exports a to-do list
+information for a given employee ID to JSON format."""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todo = requests.get(url + "todos", pms={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todo]}, jsonfile)
