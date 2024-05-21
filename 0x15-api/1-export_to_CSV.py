d3fae03dfd494d76c02bab19e2c2065fa9fe1b0c
Alaa Okair <alaaokair330@gmail.com>
2024-05-20 18:54:10 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" A script that exports a to-do list information for a given employee ID to CSV format."""
+""" A script that exports a to-do list
+information for a given employee ID to CSV format."""
 
 import csv
 import requests
@@ -14,4 +15,6 @@ if __name__ == "__main__":
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, t.get("completed"), t.get("title")]) for t in todo]
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todo]
