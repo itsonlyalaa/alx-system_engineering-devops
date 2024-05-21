05b00f7eb4cef071e7a14831772f3c1efa819f3e
Alaa Okair <alaaokair330@gmail.com>
2024-05-20 18:34:22 -0700

    tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A script that returns a to-do list information for a given employee ID. """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todos",
+                         parameters={"userId": sys.argv[1]}).json()
+    completed = [t.get("title") for t in todo if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+                user.get("name"), len(completed), len(todo)))
+    [print("\t {}".format(c)) for c in completed]
